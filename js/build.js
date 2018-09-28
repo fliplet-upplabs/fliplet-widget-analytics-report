76022308c7a8f581d32c9e0ae0ac6c912372b509
Tony Wu <twu@fliplet.com>
2018-09-28 16:16:41 -0700

    Publicise analytics report widget with JS API

--- js/build.js
+++ js/build.js
@@ -1,12 +1,48 @@
-// Include your namespaced libraries
-var AppAnalytics = new Fliplet.Registry.get('comflipletanalytics-report:1.0:core');
+(function () {
+  // Include your namespaced libraries
+  var AnalyticsReport = new Fliplet.Registry.get('comflipletanalytics-report:1.0:core');
+  var analyticsReports = {};
 
-// This function will run for each instance found in the page
-Fliplet.Widget.instance('comflipletanalytics-report-1-0-0', function (data) {
-  // The HTML element for each instance. You can use $(element) to use jQuery functions on it
-  var element = this;
+  // This function will run for each instance found in the page
+  Fliplet.Widget.instance('comflipletanalytics-report-1-0-0', function (data) {
+    // The HTML element for each instance. You can use $(element) to use jQuery functions on it
+    var element = this;
 
-  // Sample implementation to initialise the widget
-  var appAnalytics = new AppAnalytics(element, data);
-  appAnalytics.start();
-});
+    // Sample implementation to initialise the widget
+    var analyticsReport = new AnalyticsReport(element, data);
+    analyticsReport.start();
+    analyticsReports[data.id] = analyticsReport;
+  });
+
+  Fliplet.Widget.register('AnalyticsReport', function () {
+    function getReport(options) {
+      if (!Object.keys(analyticsReports).length) {
+        throw new Error('No reports found.');
+      }
+
+      options = options || {};
+      var id = options.id;
+      if (Object.keys(analyticsReports).length > 1 && !id) {
+        throw new Error('There are multiple reports on the page. An id must be provided.');
+      }
+
+      var report;
+
+      if (Object.keys(analyticsReports).length === 1) {
+        report = analyticsReports[Object.keys(analyticsReports)[0]];
+      } else {
+        report = analyticsReports[id];
+      }
+
+      if (!report) {
+        throw new Error('Report not found. Please use a different id.');
+      }
+
+      return report;
+    }
+
+    return {
+      getReport: getReport
+    };
+  });  
+})();
